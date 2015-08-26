58e404908761f68f7080302afa157d019a9eb321
Saurabh Bhatia <saurabh@microsoft.com>
2015-08-25 20:17:06 -0700

    comment out update

--- LinuxDevBox/Templates/install_desktop.sh
+++ LinuxDevBox/Templates/install_desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y update
+#sudo apt-get -y update
 sudo apt-get -y install xrdp
 sudo apt-get -y install ubuntu-desktop 
 sudo /etc/init.d/xrdp start
