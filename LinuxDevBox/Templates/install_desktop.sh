2a164ce9db4261ef2a3395bbd30120098c973934
Saurabh Bhatia <saurabh@microsoft.com>
2015-08-26 14:48:52 -0700

    xubuntu

--- LinuxDevBox/Templates/install_desktop.sh
+++ LinuxDevBox/Templates/install_desktop.sh
@@ -2,8 +2,8 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" xrdp
-#sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" ubuntu-desktop 
-sudo apt-get install -q -y xfce4
-echo xfce-session >~/.xession
-#sudo /etc/init.d/xrdp start
-sudo service xrdp restart
+sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" xubuntu-desktop 
+#sudo apt-get install -q -y xfce4
+#echo xfce-session >~/.xession
+sudo /etc/init.d/xrdp start
+#sudo service xrdp restart
