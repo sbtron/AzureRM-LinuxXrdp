70efa1c8ec33c618a38a085faca8cb1957725c6e
Saurabh Bhatia <saurabh@microsoft.com>
2015-08-26 14:14:34 -0700

    more script updates

--- LinuxDevBox/Templates/install_desktop.sh
+++ LinuxDevBox/Templates/install_desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-#sudo apt-get -y update
-sudo apt-get -y install xrdp
-sudo apt-get -y install ubuntu-desktop 
+sudo apt-get update -q
+sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" xrdp
+sudo apt-get install -q -y o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" ubuntu-desktop 
 sudo /etc/init.d/xrdp start
